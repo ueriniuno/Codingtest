383728092eca57de117cd1015b9a63c4b252098b
ueriniuno <132548428+ueriniuno@users.noreply.github.com>
2024-09-15 19:59:57 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;   
+    while(1){
+        cin >> a >> b >> c;
+        
+        if(a==0 && b==0 && c==0) {break;}
+        if(a*a + b*b == c*c || c*c + b*b == a*a || a*a + c*c == b*b) {
+            cout << "right" << endl;
+        }
+        else {
+            cout << "wrong" << endl;
+        }
+    }
+}
